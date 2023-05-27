2f9af26839675eed7db7b562e5b4c846c1334a3d
cchouhan97 <113726878+cchouhan97@users.noreply.github.com>
2023-05-26 19:44:53 -0700

    clock-in.js

--- api/clock-in.js
+++ api/clock-in.js
@@ -13,5 +13,36 @@ const clockInsTable = base('Clock Ins');
 module.exports = async (req, res) => {
     const employeeId = req.query.employeeId;
 
-    // Rest of your code goes here...
-}
+    try {
+        // Load employee data
+        const employeeRecords = await employeesTable.select({
+            filterByFormula: `{Employee ID} = "${employeeId}"`,
+        }).all();
+
+        if (employeeRecords.length === 0) {
+            return res.status(404).send(`<p style="color:red;">No employee found with ID ${employeeId}</p>`);
+        }
+
+        const employee = employeeRecords[0];
+        const clockInStatus = employee.fields['Clock In Status'];
+
+        // Prevent clocking in again within the same day.
+        if (clockInStatus === 'Clocked In') {
+            return res.status(403).send(`<p style="color:red;">${employee.fields.Name} is already clocked in for today</p>`);
+        }
+
+        // Update 'Clock In Status' to 'Clocked In'
+        await employeesTable.update([{ id: employee.id, fields: { 'Clock In Status': 'Clocked In' }}]);
+
+        // Create a 'Clock-Ins' record.
+        const clockInRecord = await clockInsTable.create({
+            "Employee": [employee.id],
+            "Clock In Time": new Date().toISOString()
+        });
+
+        res.send(`<p style="color:green;">Clock-in record created for ${employee.fields.Name} with ID ${clockInRecord.id}</p>`);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send(err);
+    }
+};
